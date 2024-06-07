0010d6f0fd71bcf64038ba9bf41edd30e48e2fe2
davidjose108 <david.garrido.desousa@gmail.com>
2024-06-07 19:05:19 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+fruits = {"banana", "apple", "orange" "tomato"}
+print(fruits)
+vegetables = {"carrot", "beet", "pommes" "tomato"}
+print(vegetables)
+
+fruits.add("blackberry")
+vegetables.add("radish")
+
+fruits.intersection(vegetables)
+print(fruits)
